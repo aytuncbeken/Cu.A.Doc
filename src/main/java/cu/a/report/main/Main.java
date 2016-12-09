2b7df9bc7a28ab4096bd94f15fefc8ea4ec82ed8
aytuncbeken <aytuncbeken.ab@gmail.com>
2016-12-09 18:39:00 +0300

    Step Definition Parser Added

---
+++ src/main/java/cu/a/report/main/Main.java
@@ -0,0 +1,16 @@
+package cu.a.report.main;
+
+import com.github.javaparser.ParseException;
+import java.io.IOException;
+
+/**
+ * Created by TCAYBEKEN on 9.12.2016.
+ * Main class of Cu.A.Report project
+ */
+public class Main {
+
+
+    public static void main(String[] args) throws IOException, ParseException {
+
+    }
+}
---
+++ src/main/java/cu/a/report/parser/StepDefinitionParser.java
@@ -0,0 +1,9 @@
+package cu.a.report.parser;
+
+/**
+ * Created by TCAYBEKEN on 9.12.2016.
+ */
+public class StepDefinitionParser {
+
+
+}
